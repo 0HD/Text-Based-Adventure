3a21ad595a75a823848e986f95662fa470ec6210
0HD <53002937+0HD@users.noreply.github.com>
2025-01-29 23:52:12 +0300

    Implemented Fal client

---
+++ engine/client/fal.py
@@ -0,0 +1,33 @@
+import fal_client, os
+from dotenv import load_dotenv
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.join(BASEDIR, os.pardir))
+BASEDIR = os.path.abspath(os.path.join(BASEDIR, os.pardir))
+
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
+def generate_image(prompt):
+    result = fal_client.subscribe(
+        # replace with correct id from fal.ai
+        "fal-ai/flux/schnell",
+        arguments={
+            "prompt": prompt,
+            "image_size": "landscape_16_9",
+            "num_inference_steps": 4,
+            "num_images": 1,
+            "enable_safety_checker": True,
+            "seed": 0
+        },
+    )
+
+    image = result["images"][0]["url"]
+
+    from PIL import Image
+    import requests
+    from io import BytesIO
+
+    response = requests.get(image)
+    img = Image.open(BytesIO(response.content))
+
+    return img
